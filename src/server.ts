1f643fb404b9da2810410a19b09886f6a6b2d225
lironhefcode <lironheftman@gmail.com>
2025-04-21 17:31:50 +0300

    build setup

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,19 @@
-import express, { Express } from "express";
-import cors from "cors";
-import path from "path";
-import { storyRoutes } from "./api/story/story.routes";
-import { authRoutes } from "./api/auth/auth.routes";
-import cookieParser from "cookie-parser";
-import { setupAsyncLocalStorage } from "./midlewaers/setupAls.middleware";
-import { userRoutes } from "./api/user/user.routes";
+import express, { Express } from "express"
+import cors from "cors"
+import path from "path"
+import { storyRoutes } from "./api/story/story.routes"
+import { authRoutes } from "./api/auth/auth.routes"
+import cookieParser from "cookie-parser"
+import { setupAsyncLocalStorage } from "./midlewaers/setupAls.middleware"
+import { userRoutes } from "./api/user/user.routes"
 
-const app: Express = express();
+const app: Express = express()
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.resolve("public")));
+  app.use(
+    express.static(
+      path.resolve(__dirname, "../instgaram-front/dist/instgaram-front"),
+    ),
+  )
 } else {
   const corsOptions = {
     origin: [
@@ -21,18 +25,19 @@ if (process.env.NODE_ENV === "production") {
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
-  };
-  app.use(cors(corsOptions));
+  }
+  app.use(cors(corsOptions))
 }
 
-app.use(cookieParser());
-app.use(express.json());
-app.all("*", setupAsyncLocalStorage);
+app.use(cookieParser())
+app.use(express.json())
+app.all("*", setupAsyncLocalStorage)
 
-app.use("/api/story", storyRoutes);
-app.use("/api/auth", authRoutes);
-app.use("/api/user", userRoutes);
-const port = 3000;
-app.listen(port, "0.0.0.0", () => {
-  console.log("Server is running on port: " + port);
-});
+app.use("/api/story", storyRoutes)
+app.use("/api/auth", authRoutes)
+app.use("/api/user", userRoutes)
+const PORT = process.env.PORT ? +process.env.PORT : 3000
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log("Server is running on port: " + PORT)
+})
