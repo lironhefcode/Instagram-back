a83f3ba5d17aea42aa883bd79cd7574f8015309c
lironhefcode <lironheftman@gmail.com>
2025-04-21 20:50:57 +0300

    remove / from front addres

--- src/server.ts
+++ src/server.ts
@@ -16,7 +16,7 @@ const corsOptions = {
     "http://localhost:3000",
     "http://127.0.0.1:4200",
     "http://localhost:4200",
-    "https://instagram-front-ok8y.onrender.com/",
+    "https://instagram-front-ok8y.onrender.com",
   ],
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
